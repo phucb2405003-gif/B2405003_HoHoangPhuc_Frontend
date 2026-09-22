294a0f53a2fd89a9ad1bd78a5f7ec4d5727e5ec4
Phúc <phucb2405003@student.ctu.edu.vn>
2026-09-22 13:49:16 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
+import NotFound from "@/views/NotFound.vue";
 
 const routes = [
     {
         path: "/",
         name: "contactbook",
         component: ContactBook,
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: NotFound,
     },
 ];
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="page">
+        <h4>Không tìm thấy trang</h4>
+        <p>Xin lỗi, trang bạn yêu cầu không tồn tại.</p>
+    </div>
+</template>
+
+<style>
+.page {
+    max-width: 400px;
+    margin: auto;
+}
+</style>
